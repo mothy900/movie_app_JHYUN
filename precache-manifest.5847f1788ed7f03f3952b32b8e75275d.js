703afd0ca726aa00ea1da2d6dfd0ebbcda92dafa
mothy900 <mothy900@naver.com>
2020-09-06 17:59:31 +0900

    Updates

---
+++ precache-manifest.5847f1788ed7f03f3952b32b8e75275d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca5bd488817b6a1a9b705054c9cd85a",
+    "url": "/movie_app_kofic/index.html"
+  },
+  {
+    "revision": "4f25961804f9ef1ac9c1",
+    "url": "/movie_app_kofic/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7881dbe04d490a812fe7",
+    "url": "/movie_app_kofic/static/js/2.8f180285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_kofic/static/js/2.8f180285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f25961804f9ef1ac9c1",
+    "url": "/movie_app_kofic/static/js/main.5d29f75a.chunk.js"
+  },
+  {
+    "revision": "1c97534f4af1c4a8fe6d",
+    "url": "/movie_app_kofic/static/js/runtime-main.3a8f8024.js"
+  }
+]);

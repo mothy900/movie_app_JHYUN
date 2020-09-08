b96bd2913bcaa6c2b2cd409acc6461297ef5c62a
mothy900 <mothy900@naver.com>
2020-09-08 17:29:51 +0900

    working +booking rate

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ class App extends React.Component {
     isLoading: true,
     weeklyBoxOfficeList: [],
     date: "",
+    totalAudi: "", //누적관객수 -> 예매율 계산 이거 Movie.js에서 불러내서 계산
   };
 
   handleChange = (e) => {
+    //input text창에 데이터를 입력 받도록
     this.setState({
       date: e.target.value,
     });
   };
 
   handleKeyPress = (e) => {
+    // 엔터 누르면 값 입력 받도록하는 함수
     this.state.date.slice(0, 7);
     if (this.state.date.length === 8) {
       this.inputSearchDate();
@@ -28,7 +31,7 @@ class App extends React.Component {
   };
 
   inputSearchDate = (e) => {
-    this.state.date.slice(0, 7);
+    this.state.date.slice(0, 7); // 년도 숫자가 초과될 경우 8글자로 자른다.
 
     if (this.state.date) {
       this.getmovie();
@@ -46,8 +49,14 @@ class App extends React.Component {
         boxOfficeResult: { weeklyBoxOfficeList },
       },
     } = await Axios.get(finalURL);
+    let totalAudi;
+    for (let i = 0; i < weeklyBoxOfficeList.length; i++) {
+      totalAudi += parseInt(weeklyBoxOfficeList[i].audiCnt);
+      console.log("first : " + totalAudi);
+    }
 
-    this.setState({ weeklyBoxOfficeList, isLoading: false });
+    this.setState({ weeklyBoxOfficeList, isLoading: false, totalAudi });
+    console.log("load :" + totalAudi);
     /*const requestURL =
       //http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=26f8c1b0fd8afe36ceb0eff48b468b5b&targetDt=20120101"
       const movieInfo = await fetch(requestURL);
@@ -62,7 +71,8 @@ class App extends React.Component {
   render() {
     // const { weeklyBoxOfficeList } = this.state;
     //console.log(weeklyBoxOfficeList);
-    const { isLoading, weeklyBoxOfficeList } = this.state;
+    const { isLoading, weeklyBoxOfficeList, totalAudi } = this.state;
+    console.log("totalAudi : " + totalAudi);
     return (
       <section className="container">
         <h1>Display Weekly Boxoffice Ranking</h1>
@@ -70,6 +80,7 @@ class App extends React.Component {
           <div className="loader">
             <h3>Input SearchDate </h3>
             <input
+              className="loader__serchbar"
               placeholder="Serch Date YYYYMMDD"
               value={this.state.date}
               onChange={this.handleChange}
@@ -80,9 +91,13 @@ class App extends React.Component {
           <div className="movies">
             {weeklyBoxOfficeList.map((movie) => (
               <Movie
+                key={movie.movieCd}
+                id={movie.movieCd}
                 rank={movie.rank}
                 movieNm={movie.movieNm}
                 openDt={movie.openDt}
+                audiAcc={movie.audiAcc}
+                audiCnt={(movie.audiCnt / totalAudi) * "100"}
               />
             ))}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,22 @@
 import React from "react";
-//import PropTypes from "prop-types";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ movieNm, rank, openDt }) {
+function Movie({ movieNm, rank, openDt, audiAcc, audiCnt }) {
   return (
     <div className="movie">
       <div className="movie__data">
-        <h5>ranking : {rank}</h5>
-        <h3>movie name : {movieNm}</h3>
-        <h3>open Data : {openDt}</h3>
+        <h3>Ranking : {rank}</h3>
+        <h3>Movie Name : {movieNm}</h3>
+        <h3>Open Date : {openDt}</h3>
+        <h3>Reservation Rate : {audiCnt}</h3>
+        <h3>누적 관객 수 : {audiAcc}</h3>
       </div>
     </div>
   );
 }
 
+Movie.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 export default Movie;
